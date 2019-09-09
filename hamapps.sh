2562e98bb03522adde2eb1504bc6b00d53a4cb80
Steve Magnuson <ag7gn.steve@gmail.com>
2019-09-09 13:34:15 -0700

    Modified help text

--- hamapps.sh
+++ hamapps.sh
@@ -16,7 +16,7 @@
 #
 #=========================================================================================
 
-VERSION="1.56.1"
+VERSION="1.56.2"
 
 GITHUB_URL="https://github.com"
 HAMLIB_LATEST_URL="$GITHUB_URL/Hamlib/Hamlib/releases/latest"
--- updatepi.sh
+++ updatepi.sh
@@ -3,7 +3,7 @@
 # YAD/shell script to install or update certain ham applications, as well as 
 # update Raspbian OS and apps.
 
-VERSION="1.56.1"
+VERSION="1.56.2"
 
 if ! command -v hamapps.sh 1>/dev/null 2>&1
 then
@@ -78,7 +78,7 @@ ANS="$(yad --center --title="Update Apps/OS - version $VERSION" --list --height=
 	--text "<b>This script will install and/or check for and install updates for the apps you select below.\n \
 If there are updates available, it will install them.</b>\n\n \
 This Pi must be connected to the Internet\nfor this script to work.\n\n \
-CLOSE ALL OTHER APPS before you click OK." \
+<span color='red'>CLOSE ALL OTHER APPS</span> before you click OK.\n" \
 --separator="," --checklist --column Pick --column Applications \
 --column Action < "$TFILE" --buttons-layout=center)"
 
