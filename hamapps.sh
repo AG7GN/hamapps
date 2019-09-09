7243b76a723edf6c133ee8d52542753cdfc028cd
Steve Magnuson <ag7gn.steve@gmail.com>
2019-09-09 14:19:50 -0700

    Add clickable links

--- hamapps.sh
+++ hamapps.sh
@@ -16,7 +16,7 @@
 #
 #=========================================================================================
 
-VERSION="1.56.3"
+VERSION="1.56.4"
 
 GITHUB_URL="https://github.com"
 HAMLIB_LATEST_URL="$GITHUB_URL/Hamlib/Hamlib/releases/latest"
